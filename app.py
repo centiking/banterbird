4fa1ee59ae6a23fe7a9e67c2d8f054c939e4105e
centiking <143682459+centiking@users.noreply.github.com>
2025-04-15 10:30:59 -0500

    Adding the Signup File

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Home.html")
+
+@app.route('/signup')
+def index():
+    return render_template("Signup.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
