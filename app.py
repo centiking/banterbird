54c3c041eba9462ce9b892a1e8b0cc8c735d76fd
centiking <143682459+centiking@users.noreply.github.com>
2025-04-23 12:09:57 -0500

    Updated

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/api/post', methods=['POST'])
+@app.route('/api/posts', methods=['POST'])
 def add_post():
     new_post = request.get_json()
-    with open("post.json", "r") as file:
+    with (open("posts.json", "r")) as file:
         posts = json.load(file)
-    posts.insert(0, new_post)
+        posts.insert(0, new_post) # add the post at the top of the file
+    with (open("posts.json", "w")) as file:
+        json.dump(posts, file, indent=4)
+    return jsonify({"status": "success"}), 201
+
 
 @app.route('/api/posts')
 def get_posts():
-    
     with open ('posts.json', 'r') as file:
         posts = json.load(file)
     return jsonify(posts)
--- static/js/app.js
+++ static/js/app.js
@@ -7,11 +7,23 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
-}
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          message: message,
+        }),
+      });
+    } catch (error) {
+      console.error("Error submitting post:", error);
+    }
+  }
 
 window.onload = async () => {
     try{
